a2e114bd4b10cf1f7d98dc169d82adf1c943d231
Sarkis Peha <sarkispeha@gmail.com>
2014-09-22 11:32:42 -0600

    initial commit, includes background and event handler

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).on('ready', function() {
+  
+var Arr =[];
+var point = 
+
+$(document).on('click', function(e) {
+	$('body').text('pageX: ' + e.pageX + ', pageY ' + e.pageY);
+
+});
+
+
+
+
+});
