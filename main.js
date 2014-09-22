c2f5e4a82d66fa13f3750c377241b10ce263e2f6
Sarkis Peha <sarkispeha@gmail.com>
2014-09-22 16:24:37 -0600

    click to place and click to remove now functional, stopPropagation

--- main.js
+++ main.js
@@ -4,21 +4,24 @@ var Arr =[];
 var xcoord = 0;
 var ycoord = 0;
 
-
-$(document).on('click', 'img', function(e) {
+$(document).on('click', '.coordinates', function(e) {
 	xcoord = (e.pageX);
 	ycoord = (e.pageY);	
 	console.log(xcoord,ycoord);
-	$('body').append('<div class="marker"><img src="map-pin.jpg"></div>');
-	$('.marker').css({
+	var pin = $('<img class="smallpin" src="map-pin.jpg">')
+	pin.css({
 		'position': 'absolute',
 		'left': xcoord,
-		'top': ycoord,
+		'top': ycoord
 		})
-
+	$('.coordinates').append(pin);
 
 });
 
+$(document).on('click', '.smallpin' , function(e){
+	e.stopPropagation()
+	$(this).remove();
+} )
 
 
 
