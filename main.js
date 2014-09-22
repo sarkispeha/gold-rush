f8ac17b76301305d9c0e313045a1a4fc6efc1bea
Sarkis Peha <sarkispeha@gmail.com>
2014-09-22 14:51:32 -0600

    x and y coordinates saved to variables, pin placed upon map, changes with every event firing

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 $(document).on('ready', function() {
   
 var Arr =[];
-var point = 
+var xcoord = 0;
+var ycoord = 0;
 
-$(document).on('click', function(e) {
-	$('body').text('pageX: ' + e.pageX + ', pageY ' + e.pageY);
+
+$(document).on('click', 'img', function(e) {
+	xcoord = (e.pageX);
+	ycoord = (e.pageY);	
+	console.log(xcoord,ycoord);
+	$('body').append('<div class="marker"><img src="map-pin.jpg"></div>');
+	$('.marker').css({
+		'position': 'absolute',
+		'left': xcoord,
+		'top': ycoord,
+		})
+
 
 });
+
+
+
 
 
 
